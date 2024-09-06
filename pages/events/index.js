0d4cb290b066a644dbe2ed2f4a0705a0738df04e
Sundze <keyonmwesundze@mail.com>
2024-09-06 03:32:46 +0100

    filter component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,24 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+import { getAllEvents } from "../../dummy-data";
+import EvenList from "../../components/events/event-list";
+import EventSearch from "../../components/events/events-search";
+
 function AllEventsPage() {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <Fragment>
+      <EventSearch onSearch={findEventsHandler} />
+      <EvenList items={events} />
+    </Fragment>
   );
 }
 
